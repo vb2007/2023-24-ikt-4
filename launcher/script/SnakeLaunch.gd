ce419b29a0874ac16ac7a50ab888fbca347a95f6
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-06-03 21:56:25 +0200

    refactored launcher code from godot v3.2 to godot v4.2.2 + added gitignore for minesweeper

--- launcher/script/SnakeLaunch.gd
+++ launcher/script/SnakeLaunch.gd
@@ -1,51 +1,50 @@
 extends Button
 
-#link a GitHub-os release-hez
-var exeLink = "https://github.com/vb2007/2023-2024-ikt-4/release..."
-var exePath = "user://snake.exe"
+# Link to the GitHub release
+@export var exe_link := "https://github.com/vb2007/2023-2024-ikt-4/release..."
+@export var exe_path := "user://snake.exe"
 
-var httpRequest = HTTPRequest
+var http_request: HTTPRequest
 
-func _ready():
+func _ready() -> void:
 	self.disabled = true
 
-func fileExists(path: String) -> bool:
-	var dir = Directory.new() #lehet váltani kell EditorFileSystemDirectory.new()-ra
+func file_exists(path: String) -> bool:
+	var dir := DirAccess.open("res://")  # Use DirAccess instead of Directory
 	return dir.file_exists(path)
 
-func verifyGamefiles():
-	if fileExists(exePath):
+func verify_gamefiles() -> void:
+	if file_exists(exe_path):
 		pass
 	else:
 		pass
 
-func downloadFile(link: String, path: String):
-	httpRequest = HTTPRequest.new()
-	add_child(httpRequest)
-	
-	self.text = "Downloading " + str(path.get_file())
-	httpRequest.connect("request_completed", self, "installFile", path)
-	
-	#hibakezelés
-	var error = httpRequest.request_raw(link)
+func download_file(link: String, path: String) -> void:
+	http_request = HTTPRequest.new()
+	add_child(http_request)
+
+	self.text = "Downloading " + path.get_file()
+	http_request.request_completed.connect(install_file.bind(path))  # Use .bind() for arguments
+
+	# Error handling
+	var error := http_request.request(link)
 	if error != OK:
 		self.text = "Download Error: " + str(error)
 
-#amikor a letöltés kész...
-func installFile(result, reponseCode, headers, body, path):
-	var dir = Directory.new()
+# When the download is complete...
+func install_file(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray, path: String) -> void:
+	var dir := DirAccess.open("res://")  # Use DirAccess instead of Directory
 	dir.remove(path)
-	
-	var file = File.new()
-	file.open(path, File.WRITE)
+
+	var file := FileAccess.open(path, FileAccess.WRITE)
 	file.store_buffer(body)
 	file.close()
 
-func checkIntergrity():
-	if !fileExists(exePath):
-		downloadFile(exeLink, exePath)
+func check_integrity() -> void:
+	if not file_exists(exe_path):
+		download_file(exe_link, exe_path)
 		print("No .exe file found.")
 		return
-	
+
 	self.text = "Start game"
 	self.disabled = false
